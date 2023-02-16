915a3d70fe656f18a4e60a50557ecdf01af11ade
kairat <jumanov7070@gmail.com>
2023-02-16 17:33:47 +0600

    project configuration

---
+++ src/i18next.js
@@ -0,0 +1,24 @@
+import i18next from "i18next";
+import HttpBackend from "i18next-http-backend";
+import LanguageDetector from "i18next-browser-languagedetector";
+import { initReactI18next } from "react-i18next";
+
+const apiKey = "xMOajJs220K1irbYM0_HJg";
+const loadPath = `https://api.i18nexus.com/project_resources/translations/{{lng}}/{{ns}}.json?api_key=${apiKey}`;
+
+i18next
+  .use(HttpBackend)
+  .use(LanguageDetector)
+  .use(initReactI18next)
+  .init({
+    fallbackLng: "ky",
+
+    ns: ["default"],
+    defaultNS: "default",
+
+    supportedLngs: ["ky","ru"],
+    
+    backend: {
+      loadPath: loadPath
+    }
+  })
